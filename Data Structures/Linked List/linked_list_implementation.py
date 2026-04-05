b401c34bfe579a48057de693a76bb649a5b92e43
Nafisha-del <nafishamoin7@gmail.com>
2026-04-04 22:04:20 -0400

    delete node in Linked List

--- Data Structures/Linked List/linked_list_implementation.py
+++ Data Structures/Linked List/linked_list_implementation.py
@@ -1,5 +1,6 @@
 # Linked List Implementation using Python with:
-## 1. Finding the lowest value in Linked List (findLowestValue)
+## 1. Finding the lowest value (findLowestValue)
+## 2. Delete a specific node (deleteNode)
 class Node:
     def __init__(self, data):
        self.data = data
@@ -7,6 +8,8 @@ class Node:
        
 # Find Lowest Value 
 def findLowestValue(head):
+    if head is None:   
+        return None
     minValue = head.data
     currentNode = head.next
     while currentNode:
@@ -14,6 +17,32 @@ def findLowestValue(head):
             minValue = currentNode.data
         currentNode = currentNode.next 
     return minValue
+
+# Delete Specific Node
+def deleteNode(head, nodeToDelete):
+    if head is None:  
+        return None
+    # If head needs to be deleted
+    if head == nodeToDelete:
+        return head.next 
+    current = head
+    while current.next and current.next!=nodeToDelete:
+        current = current.next 
+    # Node not found
+    if current.next is None:
+        print("Node not found")
+        return head
+    # Delete node
+    current.next = current.next.next
+    return head
+
+# Helper function to print list
+def printList(head):
+    temp = head
+    while temp:
+        print(temp.data, end=" -> ")
+        temp = temp.next
+    print("None")
 
 node1 = Node(7)
 node2 = Node(11)
@@ -26,4 +55,15 @@ node2.next = node3
 node3.next = node4
 node4.next = node5
 
-print("The lowest value in the linked list is:", findLowestValue(node1))
+# --------- Test ---------
+print("Original List:")
+printList(node1)
+
+# Find minimum
+print("Lowest value:", findLowestValue(node1))
+
+# Delete a node (example: delete node3 → value 3)
+node1 = deleteNode(node1, node3)
+
+print("After deleting 3:")
+printList(node1)
